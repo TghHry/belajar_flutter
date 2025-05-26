f864ca7463f4f49bd8031c45a667f2041054aef6
teguh <hariyantoteguh271@gmail.com>
2025-05-26 11:50:17 +0700

    Tugas_6_flutter

--- lib/meet_6/tugas_6_flutter.dart
+++ lib/meet_6/tugas_6_flutter.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class TugasEnam extends StatefulWidget {
   const TugasEnam({super.key});
@@ -8,122 +9,219 @@ class TugasEnam extends StatefulWidget {
 }
 
 class _TugasEnamState extends State<TugasEnam> {
+  bool _obscureTextA = true;
+  final TextEditingController usernamecontroller = TextEditingController();
+  final TextEditingController passwordcontroller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF001F54),
+      backgroundColor: const Color(0xFF001F54), // Navy blue
       appBar: AppBar(
-        backgroundColor: Color(0xFF001F54),
-        centerTitle: true,
-        title: Text(
-          "Login",
-          textAlign: TextAlign.center,
-          style: TextStyle(color: Colors.white, fontSize: 20),
-        ),
+        backgroundColor: const Color(0xFF001F54),
         elevation: 0,
+        centerTitle: true,
         leading: IconButton(
-          icon: Icon(Icons.arrow_back_ios, color: Colors.white),
-          onPressed: () {},
+          icon: const Icon(Icons.arrow_back_ios, color: Colors.white),
+          onPressed: () {
+            // Navigator.pop(context); // Kembali ke halaman sebelumnya
+          },
         ),
+        title: Text("Login", style: GoogleFonts.roboto(color: Colors.white)),
       ),
       body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 5),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  "Hello Welcome Back",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 26,
-                    fontWeight: FontWeight.bold,
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 40),
+
+              // Header Text
+              Text(
+                "Hello Welcome Back",
+                textAlign: TextAlign.center,
+                style: GoogleFonts.roboto(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 26,
+                ),
+              ),
+              const SizedBox(height: 8),
+              Text(
+                "Welcome back please\nsign in again",
+                textAlign: TextAlign.center,
+                style: GoogleFonts.roboto(color: Colors.white70, fontSize: 16),
+              ),
+
+              const SizedBox(height: 40),
+
+              // Email Field
+              TextField(
+                style: GoogleFonts.roboto(color: Colors.white),
+                controller: usernamecontroller,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  hintText: "Teguh Hariyanto",
+                  hintStyle: GoogleFonts.roboto(color: Colors.white70),
+                  prefixIcon: Icon(Icons.email, color: Colors.white70),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white10),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
                   ),
                 ),
-                SizedBox(height: 8),
-                Text(
-                  "Welcome back please\nsign in again",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.white70, fontSize: 16),
-                ),
-                SizedBox(height: 40),
-                TextField(
-                  style: TextStyle(color: Colors.white),
-                  decoration: InputDecoration(
-                    hintText: "Email",
-                    prefixIcon: Icon(Icons.email),
-                    hintStyle: TextStyle(color: Colors.white70),
-                    enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white54),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 24),
-                TextField(
-                  obscureText: true,
-                  style: TextStyle(color: Colors.white),
-                  decoration: InputDecoration(
-                    hintText: "Password",
-                    prefixIcon: Icon(Icons.lock, color: Colors.white),
-                    hintStyle: TextStyle(color: Colors.white70),
-                    enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white54),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 40),
-                //login button
-                SizedBox(
-                  width: double.infinity,
-                  height: 50,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.white,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30),
-                      ),
+              ),
+
+              const SizedBox(height: 24),
+
+              // Password Field
+              TextField(
+                controller: passwordcontroller,
+                obscureText: _obscureTextA,
+                style: GoogleFonts.roboto(color: Colors.white70),
+                decoration: InputDecoration(
+                  hintText: "Password",
+                  hintStyle: TextStyle(color: Colors.white70),
+                  prefixIcon: Icon(Icons.lock, color: Colors.white70),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _obscureTextA ? Icons.visibility_off : Icons.visibility,
+                      color: Colors.white,
                     ),
                     onPressed: () {
-                      //handle login
+                      setState(() {
+                        _obscureTextA = !_obscureTextA;
+                      });
                     },
-                    child: Text(
-                      "Login",
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        color: Color(0xFF001F54),
-                      ),
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white10),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 40),
+
+              // Login Button
+              SizedBox(
+                width: double.infinity,
+                height: 50,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                  onPressed: () {
+                    print("username : ${usernamecontroller.text}");
+                    print("password : ${passwordcontroller.text}");
+                    // TODO: Handle login
+                  },
+                  child: Text(
+                    "Login",
+                    style: GoogleFonts.roboto(
+                      color: Color(0xFF001F54),
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
                 ),
-                SizedBox(height: 30),
-                Row(
-                  children: [
-                    Expanded(child: Divider(color: Colors.white24)),
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 10),
-                      child: Text(
-                        "or",
-                        style: TextStyle(color: Colors.white70),
+              ),
+
+              const SizedBox(height: 60),
+
+              // Divider with "or"
+              Row(
+                children: [
+                  Expanded(child: Divider(color: Colors.white54)),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    child: Text(
+                      "or",
+                      style: GoogleFonts.roboto(color: Colors.white70),
+                    ),
+                  ),
+                  Expanded(child: Divider(color: Colors.white54)),
+                ],
+              ),
+
+              const SizedBox(height: 30),
+
+              // Facebook Button
+              SizedBox(
+                width: double.infinity,
+                height: 50,
+                child: ElevatedButton.icon(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white10,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                  icon: const Icon(Icons.facebook, color: Colors.white),
+                  onPressed: () {},
+                  label: const Text(
+                    "Facebook",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 16),
+
+              // Google Button
+              SizedBox(
+                width: double.infinity,
+                height: 50,
+                child: ElevatedButton.icon(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white10,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                  icon: Image.asset(
+                    "assets/images/google3.png",
+                    height: 14,
+                  ),
+                  onPressed: () {},
+                  label: Text(
+                    "Gmail",
+                    style: GoogleFonts.roboto(color: Colors.white),
+                  ),
+                ),
+              ),
+              SizedBox(height: 30),
+
+              // Sign up link
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Already have an account?",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      // TODO : HAndle sign up
+                    },
+                    child: Text(
+                      "   Sign In",
+                      style: GoogleFonts.roboto(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold,
+                        decoration: TextDecoration.underline,
                       ),
                     ),
-                    Expanded(child: Divider(color: Colors.white24)),
-                  ],
-                ),
-                //facebook
-                
-
-              ],
-            ),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
